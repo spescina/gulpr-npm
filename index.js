b11fdbc779199fcbe2108643eca2b0ca043f41ab
Simone Pescina <s.pescina@gmail.com>
2015-11-04 16:22:52 +0100

    no message

---
+++ index.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp'),
+    sass = require('./tasks/sass'),
+    watch = require('./tasks/watch'),
+    lint = require('./tasks/lint'),
+    vendor = require('./tasks/vendor'),
+    styles = require('./tasks/styles'),
+    scripts = require('./tasks/scripts'),
+    cacheBust = require('./tasks/cacheBust'),
+    assetConfig = require('./Config');
+
+gulp.task('lint', lint(assetConfig));
+gulp.task('vendor', vendor(assetConfig));
+gulp.task('sass', sass(assetConfig));
+gulp.task('styles', styles(assetConfig));
+gulp.task('scripts', scripts(assetConfig));
+gulp.task('cacheBust', cacheBust(assetConfig));
+
+gulp.task('watch', watch(assetConfig));
+
+gulp.task('build', ['lint', 'sass', 'styles', 'scripts', 'cacheBust']);
