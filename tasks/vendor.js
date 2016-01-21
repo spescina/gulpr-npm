39bf0880a92ea2d5773f9c3184e2cb67a8b0146c
Simone Pescina <s.pescina@gmail.com>
2016-01-21 17:36:32 +0100

    gestione dei menu di navigazione innetstati drag & drop. serve un copioso refactoring dell'area js anche perchè attualmente tutto il js è in puppa e non nei relativi sottomoduli [serve un composer dump-autoload, un npm install e un gulp vendor]

--- tasks/vendor.js
+++ tasks/vendor.js
@@ -1,36 +1,57 @@
 var gulp = require('gulp'),
     path = require('path'),
+    fsExtra = require('fs-extra'),
     rimraf = require('rimraf'),
+    q = require('q'),
     _ = require('lodash'),
     Logger = require('../Logger'),
     resolve = require('../PathResolver'),
     config;
 
-var clean = function() {
-    rimraf(resolve(config.vendor.dest), function(){});
+var clean = function () {
+    var deferred = q.defer();
+
+    rimraf(resolve(config.vendor.dest), function () {
+        deferred.resolve();
+    });
+
+    return deferred.promise;
 };
 
 var buildVendorGlob = function (vendor) {
-    return path.join(process.cwd(), 'node_modules', vendor, '**/*');
+    return path.resolve(path.join(process.cwd(), 'node_modules', vendor));
 };
 
 var buildVendorDest = function (vendor) {
-    return path.join(resolve(config.vendor.dest), vendor);
+    return path.resolve(path.join(resolve(config.vendor.dest), vendor));
 };
 
 var clone = function () {
+    var queue = config.vendor.packages.length;
+    var deferred = q.defer();
+
     _.each(config.vendor.packages, function (vendor) {
-        gulp.src(buildVendorGlob(vendor))
-            .pipe(gulp.dest(buildVendorDest(vendor)));
+        fsExtra.copy(buildVendorGlob(vendor), buildVendorDest(vendor), function () {
+            Logger.message(vendor + ' ... done!');
+            queue--;
+
+            if (!queue) {
+                deferred.resolve();
+            }
+        });
     });
+
+    return deferred.promise;
 };
 
 var vendorTask = function () {
     Logger.heading('Vendor packages');
     Logger.message('cleaning ...');
-    clean();
-    Logger.message('cloning ...');
-    return clone();
+
+    return clean().then(function () {
+        Logger.message('cloning ...');
+        return clone();
+    });
 };
 
 var task = function (configFile) {
