44390bbc905d92e70c1541cca71a3c5f7eaa07c3
Simone Pescina <s.pescina@gmail.com>
2016-02-23 10:46:46 +0100

    sistemata procedura di build

--- tasks/copy.js
+++ tasks/copy.js
@@ -1,29 +1,11 @@
 var gulp = require('gulp'),
     path = require('path'),
     fsExtra = require('fs-extra'),
-    rimraf = require('rimraf'),
     q = require('q'),
     _ = require('lodash'),
     Logger = require('../Logger'),
     resolve = require('../PathResolver'),
     config;
-
-var clean = function () {
-    var queue = config.copy.length;
-    var deferred = q.defer();
-
-    _.each(config.copy, function (copy) {
-        rimraf(resolve(copy.dest), function () {
-            queue--;
-
-            if (!queue) {
-                deferred.resolve();
-            }
-        });
-    });
-
-    return deferred.promise;
-};
 
 var buildCopyGlob = function (copy) {
     return path.resolve(resolve(copy.source));
@@ -55,10 +37,8 @@ var copyTask = function () {
     Logger.heading('Copy static assets');
     Logger.message('cleaning ...');
 
-    return clean().then(function () {
-        Logger.message('copying ...');
-        return clone();
-    });
+    Logger.message('copying ...');
+    return clone();
 };
 
 var task = function (configFile) {
